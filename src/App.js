f01d9a839e1899cace5f181cf80b1c96630b9682
jaquesaurelio2 <jaquesaurelio1@gmail.com>
2024-06-29 16:01:00 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/**
+  - O componente App deve renderizar a soma das constantes a e b 
+  e seu resultado no seguinte formato: 1 + 1 = 2
+*/
+
+export default function App() {
+  const a = 1;
+  const b = 1;
+
+  return (
+    <div className="App">
+      <div>1 + 1 = 2</div>
+    </div>
+  );
+}
